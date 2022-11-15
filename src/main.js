fd5dd019fa2da6a50c011a1154f57275c35041be
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2022-11-15 14:49:26 +0700

    init pages

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue, { createApp } from 'vue'
-import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import './assets/main.css'
-
-Vue.use(BootstrapVue);
-createApp(App).mount('#app')
